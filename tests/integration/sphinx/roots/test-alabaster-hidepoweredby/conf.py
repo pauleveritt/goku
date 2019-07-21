acb085e4634c2d283c6e8ab76ee569c32c455d61
Paul Everitt <pauleveritt@me.com>
2019-07-21 11:29:56 -0400

    Test coverage for hiding poweredby

---
+++ tests/integration/sphinx/roots/test-alabaster-hidepoweredby/conf.py
@@ -0,0 +1,13 @@
+extensions = ['goku']
+html_theme = 'goku'
+
+# The rest is normal Sphinx stuff
+master_doc = 'index'
+html_title = ''
+exclude_patterns = ['_build']
+project = 'Goku Sidebars'
+
+html_static_path = ['_static']
+html_theme_options = dict(
+    show_powered_by=False
+)
